53388d611f0d0ae81062c93b71cd4215220d0727
gabriel <gabriel@gabriel-Lenovo-Z50-70>
2016-03-04 16:35:51 -0600

    Hash

---
+++ xC++2/src/ManejoDatos/main.h
@@ -0,0 +1,17 @@
+/*
+ * main.h
+ *
+ *  Created on: 4 de mar. de 2016
+ *      Author: gabriel
+ */
+
+#ifndef SRC_MANEJODATOS_MAIN_H_
+#define SRC_MANEJODATOS_MAIN_H_
+
+class main {
+public:
+	main();
+	virtual ~main();
+};
+
+#endif /* SRC_MANEJODATOS_MAIN_H_ */
