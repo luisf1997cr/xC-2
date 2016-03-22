78a9878667ce57ab6b3c22a917a7b8ecf872b11b
luisf1997cr <luismuelino@gmail.com>
2016-03-21 20:00:06 -0600

    conexion

---
+++ xC++2/src/conexion/cliente/client.cpp
@@ -0,0 +1,48 @@
+/*
+   client.cpp
+
+   Test client for the tcpsockets classes. 
+
+   ------------------------------------------
+
+   Copyright � 2013 [Vic Hargrave - http://vichargrave.com]
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+#include "tcpconnector.h"
+
+using namespace std;
+
+int main()
+{
+    int len;
+    string message;
+    char line[256];
+    TCPConnector* connector = new TCPConnector();
+    TCPStream* stream = connector->connect("127.0.0.1",8080);
+    if (stream) {
+        message = "Sirve la conexion?";
+        stream->send(message.c_str(), message.size());
+        printf("sent - %s\n", message.c_str());
+        len = stream->receive(line, sizeof(line));
+        line[len] = 0;
+        printf("received - %s\n", line);
+        delete stream;
+    }
+
+    exit(0);
+}
