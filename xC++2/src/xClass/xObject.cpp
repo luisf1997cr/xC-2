dc431900ff9a65e04ca539f1bcfde8c98dd06e89
jejaeddy <jeja12@live.com>
2016-03-23 09:01:12 -0600

    Agrego metodos del xObject

--- xC++2/src/xClass/xObject.cpp
+++ xC++2/src/xClass/xObject.cpp
@@ -7,6 +7,24 @@
 
 #include "xObject.h"
 
+long int xObject::_Id = 0;
+
+xObject::xObject(){
+	this->_Id++;
+}
+
+void xObject::setId(long int pID){
+	this->_Id = pID;
+}
+
+long int xObject::getId(){
+	return this->_Id;
+}
+
+xObject::~xObject(){
+
+}
+
 xJson* xObject::serialize(){
 	//Json::Value valorJson;
 	Json::StyledWriter escritor;
