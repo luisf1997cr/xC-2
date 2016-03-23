fca345ea4471704da36c915fe6c2c4b3a64d4657
jejaeddy <jeja12@live.com>
2016-03-23 09:17:13 -0600

    Ac

--- xC++2/src/xClass/xPointer.cpp
+++ xC++2/src/xClass/xPointer.cpp
@@ -1,21 +1,100 @@
 /*
  * xPointer.cpp
  *
- *  Created on: 2 de mar. de 2016
- *      Author: gabriel
+ *  Created on: 20 de mar. de 2016
+ *      Author: jose
  */
 
 #include "xPointer.h"
 
-
-/*
-xPointer::xPointer() {
-	// TODO Auto-generated constructor stub
-
+xPointer::xPointer(){
+	this->_Reference = 1;
+	this->_LimitPointer = NULL;
 }
-
-xPointer::~xPointer() {
+void xPointer::operator =(const xPointer pXPointer){
+	this->_TopPointer = pXPointer._TopPointer;
+	this->_LimitPointer = pXPointer._LimitPointer;
+}
+//template<class AnyType>
+//void xPointer<AnyType>::operator =(AnyType pData){
+//	xMemoryManager MM;
+//	MM.xSet(*this,pData);
+//	//xMemoryManager::xSet(*this,pData);
+//}
+xObject* xPointer::operator ->(){
+	void* ptr = this->_TopPointer;
+	xObject* dato = (xObject*) ptr;
+	return dato;
+}
+void xPointer::operator ++(){
+	int size = ((int*)this->_LimitPointer - (int*)this->_TopPointer) * 4;
+	this->_TopPointer = this->_TopPointer + size;
+	this->_LimitPointer = this->_LimitPointer + size;
+}
+void xPointer::operator --(){
+	int size = ((int*)this->_LimitPointer - (int*)this->_TopPointer) * 4;
+	this->_TopPointer = this->_TopPointer - size;
+	this->_LimitPointer = this->_LimitPointer - size;
+}
+void xPointer::operator -(int pCant){
+	int size = ((int*)this->_LimitPointer - (int*)this->_TopPointer) * 4;
+	this->_TopPointer = this->_TopPointer - size * pCant;
+	this->_LimitPointer = this->_LimitPointer - size * pCant;
+}
+void xPointer::operator +(int pCant){
+	int size = ((int*)this->_LimitPointer - (int*)this->_TopPointer) * 4;
+	this->_TopPointer = this->_TopPointer + size * pCant;
+	this->_LimitPointer = this->_LimitPointer + size * pCant;
+}
+void xPointer::operator -=(int pCant){
+	int size = ((int*)this->_LimitPointer - (int*)this->_TopPointer) * 4;
+	this->_TopPointer = this->_TopPointer - size * pCant;
+	this->_LimitPointer = this->_LimitPointer - size * pCant;
+}
+void xPointer::operator +=(int pCant){
+	int size = ((int*)this->_LimitPointer - (int*)this->_TopPointer) * 4;
+	this->_TopPointer = this->_TopPointer + size * pCant;
+	this->_LimitPointer = this->_LimitPointer + size * pCant;
+}
+bool xPointer::operator ==(xPointer pXPointer){
+	if(this->_TopPointer == pXPointer.getTopPointer()){
+		return true;
+	}
+	return false;
+}
+bool xPointer::operator !=(xPointer pXPointer){
+	if(this->_TopPointer == pXPointer.getTopPointer()){
+		return false;
+	}
+	return true;
+}
+xObject xPointer::operator *(){
+	void* ptr = this->_TopPointer;
+	xObject dato = *(xObject*) ptr;
+	return dato;
+}
+void xPointer::setTopPointer(void* pTopPointer){
+	this->_TopPointer = pTopPointer;
+}
+void* xPointer::getTopPointer(){
+	return this->_TopPointer;
+}
+void xPointer::setLimitPointer(void* pLimitPointer){
+	this->_LimitPointer = pLimitPointer;
+}
+void* xPointer::getLimitPointer(){
+	return this->_LimitPointer;
+}
+void xPointer::setReference(int pReference){
+	this->_Reference = pReference;
+}
+int xPointer::getReference(){
+	return this->_Reference;
+}
+xPointer::~xPointer(){
 	// TODO Auto-generated destructor stub
 }
 
-**/
+
+
+
