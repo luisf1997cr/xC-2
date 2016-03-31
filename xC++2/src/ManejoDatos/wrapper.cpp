0df1bf02545ab5ea9d6863ec096b54b9e4f7c8bd
jejaeddy <jeja12@live.com>
2016-03-31 17:56:48 -0600

    Actualizacion wrapper.cpp

--- xC++2/src/ManejoDatos/wrapper.cpp
+++ xC++2/src/ManejoDatos/wrapper.cpp
@@ -7,24 +7,95 @@
 #include "wrapper.h"
 
 
-
-xInt::xInt() {
-	this->valor = 0;
+//INT
+xInt::xInt(int pDato) {
+	this->_Valor = pDato;
 }
 
 int xInt::intValue() {
-	return this->valor;
+	return this->_Valor;
 }
 
-void* xInt::operator new(size_t pSize) {
-	xPointer ptr = xMemoryManager::Instance()->xMalloc(pSize);
-	cout << "hola"  << endl;
-	int* Int = new(ptr.getTopPointer()) int();
-	cout << "xMalloc: " << ptr.getTopPointer() << endl;
-	return ptr.getTopPointer();
-}
 
 xInt::~xInt(){
-	delete &this->valor;
+	//memset(this, 0, sizeof(xInt));
 }
 
+//FLOAT
+xFloat::xFloat(float pDato){
+	this->_Valor = pDato;
+}
+
+float xFloat::floatValue(){
+	return this->_Valor;
+}
+
+xFloat::~xFloat(){
+	delete &this->_Valor;
+}
+
+//LONG
+xLong::xLong(long pDato){
+	this->_Valor = pDato;
+}
+
+long xLong::longValue(){
+	return this->_Valor;
+}
+
+xLong::~xLong(){
+	delete &this->_Valor;
+}
+
+//SHORT
+xShort::xShort(short pDato){
+	this->_Valor = pDato;
+}
+
+short xShort::shortValue(){
+	return this->_Valor;
+}
+
+xShort::~xShort(){
+	delete &this->_Valor;
+}
+
+
+//BOOL
+xBool::xBool(bool pDato){
+	this->_Valor = pDato;
+}
+
+bool xBool::boolValue(){
+	return this->_Valor;
+}
+
+xBool::~xBool(){
+	delete &this->_Valor;
+}
+
+//DOUBLE
+xDouble::xDouble(double pDato){
+	this->_Valor = pDato;
+}
+
+double xDouble::doubleValue(){
+	return this->_Valor;
+}
+
+xDouble::~xDouble(){
+	delete &this->_Valor;
+}
+
+//CHAR
+xChar::xChar(char pDato){
+	this->_Valor = pDato;
+}
+
+char xChar::charValue(){
+	return this->_Valor;
+}
+
+xChar::~xChar(){
+	delete &this->_Valor;
+}
