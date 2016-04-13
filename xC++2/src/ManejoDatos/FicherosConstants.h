3955aaaed062be5f0da4e8b2c0060265ac272539
stevensg97 <maiderr97@gmail.com>
2016-04-12 21:28:11 -0600

    Constantes de la clase Ficheros

---
+++ xC++2/src/ManejoDatos/FicherosConstants.h
@@ -0,0 +1,16 @@
+/*
+ * FicherosConstants.h
+ *
+ *  Created on: 12 de abr. de 2016
+ *      Author: steven
+ */
+
+#ifndef FICHEROSCONSTANTS_H_
+#define FICHEROSCONSTANTS_H_
+
+class FicherosConstants {
+public:
+	const char* ERROR_AL_PARSEAR = "Error al parsear xJson";
+};
+
+#endif /* FICHEROSCONSTANTS_H_ */
