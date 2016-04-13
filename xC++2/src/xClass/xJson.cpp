eb6d50255748e31ca886e9ac01a4827f56502910
stevensg97 <maiderr97@gmail.com>
2016-04-12 22:23:16 -0600

    Actualizacion

--- xC++2/src/xClass/xJson.cpp
+++ xC++2/src/xClass/xJson.cpp
@@ -10,49 +10,15 @@ using namespace std;
 
 
 
-xJson* xJson::serialize(xObject pObjeto){
-
-	switch (include_char){
-		case "xInt":
-			Json::Value valorJson;
-			Json::StyledWriter escritor;
-			//serializacion para xObject de tipo xInt
-			break;
-
-		case "xChar":
-			//serializacion para xObject de tipo xChar
-			break;
-	}
-
-
-	//valorJson["Valor"]= pObjecto.valor;
-	//valorJson["ID"]= pObjecto.id;
-
-	string sObjeto=escritor.write(valorJson);
-
-}
-
-bool xJson::deserialize(xJson* pJson){
-	//string strJson= pJson;
-
-	// Let's parse it
-	Json::Value raiz;
-	Json::Reader lector;
-	//bool parsedSuccess = lector.parse(strJson,raiz,false);
-
-	//if(not parsedSuccess){
-	// Report failures and their locations
-	// in the document.
-	cout<<"Failed to parse JSON"<<endl
-	<<lector.getFormatedErrorMessages()
-	<<endl;
-	return 1;
-	}
-
-xJson::xJson() {
+xJson::xJson(string pString) {
+	this->_StringJson = pString;
 
 }
 
 xJson::~xJson() {
 	// TODO Auto-generated destructor stub
+}
+
+string xJson::getString(){
+	return xJson::_StringJson;
 }
