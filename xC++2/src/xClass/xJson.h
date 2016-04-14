b49c193d67af26dff638343d12c57a4c53c3c934
stevensg97 <maiderr97@gmail.com>
2016-04-13 18:50:09 -0600

    Documentacion interna de xJson

--- xC++2/src/xClass/xJson.h
+++ xC++2/src/xClass/xJson.h
@@ -1,8 +1,9 @@
-/*
- * xJson.h
- *
- *  Created on: 5 de mar. de 2016
- *      Author: steven
+/**
+ * @file xJson.h
+ * @date 15/3/16
+ * @author Maider Steven Sánchez González
+ * @title xJson
+ * @brief Clase que crea los objetos xJson cuando se serializa un objeto 
  */
 
 #include <string>
@@ -17,8 +18,21 @@ class xJson {
 private:
 	string _StringJson;
 public:
+	/**
+	 * @brief Metodo costructor de xJson
+	 * @param string pString
+	 */
 	xJson(string pString);
+	
+	/**
+	 * @brief Metodo costructor de xInt
+	 */
 	virtual ~xJson();
+	
+	/**
+	 * @brief Metodo que devuelve el string en formato Json 
+	 * @return string _StringJson
+	 */
 	string getString();
 };
 
