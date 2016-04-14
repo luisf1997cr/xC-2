accb61403d3ccddafc2f7632b3e96ee254e8c11b
stevensg97 <maiderr97@gmail.com>
2016-04-13 19:48:31 -0600

    Constantes de punteros

---
+++ xC++2/src/xClass/PConstants.h
@@ -0,0 +1,17 @@
+/*
+ * PConstants.h
+ *
+ *  Created on: 13 de abr. de 2016
+ *      Author: steven
+ */
+
+#ifndef SRC_XCLASS_PCONSTANTS_H_
+#define SRC_XCLASS_PCONSTANTS_H_
+
+class PConstants {
+public:
+	const char* VALOR = " Valor: ";
+	const char* SIZE  = " Tamaño: ";
+};
+
+#endif /* SRC_XCLASS_PCONSTANTS_H_ */
