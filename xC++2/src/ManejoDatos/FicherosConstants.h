ab6dd422955513a5887051e578f1f8bab1cd0ea9
stevensg97 <maiderr97@gmail.com>
2016-04-13 20:14:18 -0600

    Update FicherosConstants.h

--- xC++2/src/ManejoDatos/FicherosConstants.h
+++ xC++2/src/ManejoDatos/FicherosConstants.h
@@ -11,6 +11,16 @@
 class FicherosConstants {
 public:
 	const char* ERROR_AL_PARSEAR = "Error al parsear xJson";
+	const char* ERROR_AL_ABRIR = "No se pudo abrir el archivo: ";
+	const char* MSJ_LEIDO = "Listo leido";
+	const char* MSJ_ESCRITO = "Se escribio";
+	const char* MSJ_ENCONTRADO = "Si se encuentra";
+	const char* MSJ_NO_ENCONTRADO = "No se encuentra";
+	const char* MSJ_SALIR = "Sali";
+	const char* MSJ_SALIR = "Sali";
+	const char* MSJ_SALIR = "Sali";
+	const char* MSJ_SALIR = "Sali";
+	const char* MSJ_SALIR = "Sali";
 };
 
 #endif /* FICHEROSCONSTANTS_H_ */
